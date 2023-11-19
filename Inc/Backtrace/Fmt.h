0dff0c2b46e0585e8368efa7bc9d47e522b59949
MarcasRealAccount <thetremine.lumicks@gmail.com>
2023-11-19 10:12:13 +0100

    Add fmt and spdlog based Asserts

--- Inc/Backtrace/Fmt.h
+++ Inc/Backtrace/Fmt.h
@@ -1,3 +1,27 @@
 #pragma once
 
 #include "Backtrace.h"
+
+#include <fmt/format.h>
+#include <string>
+#include <utility>
+
+namespace Backtrace
+{
+	template <class... Args>
+	requires(sizeof...(Args) > 0)
+	constexpr void Assert(bool statement, fmt::format_string<Args...> format, Args&&... args)
+	{
+		if constexpr (c_EnableAsserts)
+		{
+			if (statement)
+				return;
+
+			if constexpr (c_EnableBreakpoint)
+				DebugBreak();
+
+			std::string message = fmt::format(format, std::forward<Args>(args)...);
+			throw Exception("Assert", std::move(message), Capture(1));
+		}
+	}
+} // namespace Backtrace
